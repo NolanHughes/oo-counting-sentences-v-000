8862a2fc18d4a4dff631510587ebf9658de7fa1e
Nolan Hughes <nhughes987@gmail.com>
2017-11-27 23:24:29 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,19 +1,19 @@
 require 'pry'
 
-# class String
-# 
+class String
+#
 #   def sentence?
 #     self.end_with?(".")
 #   end
-# 
+#
 #   def question?
 #     self.end_with?("?")
 #   end
-# 
+#
 #   def exclamation?
 #     self.end_with?("!")
 #   end
-# 
+#
 #   def count_sentences
 #     self.split(/[.!?]/).reject do |e|
 #       e.to_s.empty?
